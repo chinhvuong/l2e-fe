752bbf3e1a591fcb270cc8b11f427127fb06d25c
Sotatek-ChinhVuong <chinhvuong@sotatek.com>
2022-07-20 23:57:53 +0700

    code base

--- layout/second-layout.tsx
+++ layout/second-layout.tsx
@@ -1,12 +1,12 @@
 import React, { ReactChild } from 'react'
-import Footer from './footer'
+import Footer from './footer/second-footer'
 import Header from './header'
 
 const Layout = ({ children }: { children: ReactChild }) => {
     return (
         <div>
             <Header />
-            <main className="limit">{children}</main>
+            <main>{children}</main>
             <Footer />
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,43 @@ module.exports = {
     // purge: ['./**/*.{js,jsx,ts,tsx}', './public/index.html'],
     content: [
         './pages/**/*.{js,ts,jsx,tsx}',
+        './layout/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
+        './containers/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {
             colors: {
-                primary: '#2962ff',
-                secondary: '#1e222d',
+                gradient: 'linear-gradient(180deg, #EE4600 0%, #FF864A 100%)',
 
-                'text-01': '#d1d4dc',
-                'text-02': '#868993',
+                pri: '#EE4600', //orange
+                'pri-52': 'rgba(238, 70, 0, 0.52)', // orange opacity 0.52
+                'pri-52': 'rgba(238, 70, 0, 0.50)', // orange opacity 0.50
+                'pri-17': 'rgba(238, 70, 0, 0.17)',
+
+                second: '#0F62F9', //blue
+                'second-17': 'rgba(15, 98, 249, 0.72)', // blue opacity 0.17
+
+                black: '#000000',
+                'black-50': 'rgba(0, 0, 0, 0.5)',
+                'black-47': 'rgba(0, 0, 0, 0.47)',
+                'black-70': 'rgba(0, 0, 0, 0.70)',
+                'black-20': 'rgba(0, 0, 0, 0.20)',
+
+                white: '#FFFFFF',
+
+                'gray-d9': '#D9D9D9',
+                'gray-d9-28': 'rgba(216, 216, 216, 0.28)',
+                'gray-d9-30': 'rgba(216, 216, 216, 0.3)',
+                'gray-d8-30': 'rgba(216, 216, 216, 0.3)',
+
+                'gray-c4': '#C4C4C4',
                 error: '#f23645',
-                'border-01': '#868993',
-                'border-02': '#2962ff',
+            },
+            boxShadow: {
+                20: '0px 2px 20px rgba(20, 19, 19, 0.05)',
+                30: '0px 2px 30px rgba(20, 19, 19, 0.05)',
+                40: '0px 2px 40px rgba(0, 0, 0, 0.05)',
             },
             screens: {
                 xl: { max: '1279px' },
