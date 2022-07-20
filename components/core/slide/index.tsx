e56ce5e95664ee82632bdda0ab5e5806bded6db5
Sotatek-ChinhVuong <chinhvuong@sotatek.com>
2022-07-20 20:25:06 +0700

    component/slide

--- components/core/slide/index.tsx
+++ components/core/slide/index.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react'
+import { Pagination } from 'swiper'
+// Import Swiper styles
+// import 'swiper/css';
+// import "swiper/css/pagination";
+// import 'swiper/swiper-bundle.css';
+import 'swiper/css/bundle'
+import './style.scss'
 
-type Props = {}
+import { ReactElement } from 'react'
 
-function Slide({}: Props) {
-    return <div>Slide</div>
+type SlideProps = {
+    data: any[]
+    // eslint-disable-next-line no-unused-vars
+    renderItem: (data: any) => ReactElement
+}
+const Slide = ({ data, renderItem, ...rest }: SlideProps & SwiperProps) => {
+    return (
+        <Swiper
+            className="custom-slide"
+            spaceBetween={28}
+            // slidesPerView={3}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            pagination={true}
+            modules={[Pagination]}
+            breakpoints={{
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 2,
+                    spaceBetween: 16,
+                },
+                // when window width is >= 480px
+                480: {
+                    slidesPerView: 3,
+                    spaceBetween: 24,
+                },
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 4,
+                    spaceBetween: 32,
+                },
+            }}
+            {...rest}
+        >
+            {data &&
+                data.length &&
+                data.map((item, index) => (
+                    <SwiperSlide key={index}>{renderItem(item)}</SwiperSlide>
+                ))}
+            {/* <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ... */}
+        </Swiper>
+    )
 }
 
 export default Slide
--- containers/home/index.tsx
+++ containers/home/index.tsx
@@ -2,10 +2,35 @@ import React from 'react'
 import './style.scss'
 import Button from '@/components/core/button'
 import Modal, { openModal } from '@/components/core/modal'
+import Slide from '@/components/core/slide'
 
 const modalId = 'modalId'
+// Import Swiper React components
 
+const data = [
+    {
+        name: 'Hello how are you',
+    },
+    {
+        name: 'Hello how are you',
+    },
+    {
+        name: 'Hello how are you',
+    },
+    {
+        name: 'Hello how are you',
+    },
+    {
+        name: 'Hello how are you',
+    },
+    {
+        name: 'Hello how are you',
+    },
+]
 const HomePageContainer = () => {
+    const renderItem = (data: any) => {
+        return <div className="h-[100px] bg-red-500">{data?.name}</div>
+    }
     return (
         <div className="">
             HomePageContainer
@@ -17,10 +42,38 @@ const HomePageContainer = () => {
                 closeIcon={true}
                 className=""
             >
-                <div className="">
+                <div className="h-[500px] bg-red-500">
                     <img src="" alt="" />
                 </div>
             </Modal>
+            <div className="p-4">
+                <Slide
+                    data={data}
+                    renderItem={renderItem}
+                    // slidesPerView={3}
+                    breakpoints={{
+                        // when window width is >= 640px
+                        640: {
+                            //   width: 640,
+                            slidesPerView: 2,
+                        },
+                        // when window width is >= 768px
+                        768: {
+                            //   width: 768,
+                            slidesPerView: 2,
+                        },
+
+                        1280: {
+                            //   width: 768,
+                            slidesPerView: 3,
+                        },
+                        1480: {
+                            //   width: 768,
+                            slidesPerView: 4,
+                        },
+                    }}
+                />
+            </div>
         </div>
     )
 }
