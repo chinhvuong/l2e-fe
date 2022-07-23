899506d6c8daf7d483fc5b571e95fd08cf63917b
ducluong <luongnd@tokyotechlab.com>
2022-07-23 18:58:01 +0700

    add cancel order data

---
+++ data/data-order.ts
@@ -0,0 +1,79 @@
+import { EStatusOrder } from '@/contants/interfaces'
+
+export const dataOrderDetails = [
+    {
+        name: 'Mặt nạ nghệ 123',
+        image: 'https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/295082524_2690799244386588_2912082838044312187_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=2K-jpp-1S9sAX-mte4L&_nc_ht=scontent.fhan14-1.fna&oh=00_AT-bHMdlW8GzLH9tbB0ixG4PIr2N8GVW_4A6wkj33yhthw&oe=62DEC24C',
+        quantity: 0,
+        cashout: 'Làng SOS',
+        price: '20.000đ',
+        total: '40.000đ',
+    },
+    {
+        name: 'Mặt nạ nghệ 123',
+        image: 'https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/295082524_2690799244386588_2912082838044312187_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=2K-jpp-1S9sAX-mte4L&_nc_ht=scontent.fhan14-1.fna&oh=00_AT-bHMdlW8GzLH9tbB0ixG4PIr2N8GVW_4A6wkj33yhthw&oe=62DEC24C',
+        quantity: 0,
+        cashout: 'Làng SOS',
+        price: '20.000đ',
+        total: '40.000đ',
+    },
+    {
+        name: 'Mặt nạ nghệ 123',
+        image: 'https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/295082524_2690799244386588_2912082838044312187_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=2K-jpp-1S9sAX-mte4L&_nc_ht=scontent.fhan14-1.fna&oh=00_AT-bHMdlW8GzLH9tbB0ixG4PIr2N8GVW_4A6wkj33yhthw&oe=62DEC24C',
+        quantity: 0,
+        cashout: 'Làng SOS',
+        price: '20.000đ',
+        total: '40.000đ',
+    },
+    {
+        name: 'Mặt nạ nghệ 123',
+        image: 'https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/295082524_2690799244386588_2912082838044312187_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=2K-jpp-1S9sAX-mte4L&_nc_ht=scontent.fhan14-1.fna&oh=00_AT-bHMdlW8GzLH9tbB0ixG4PIr2N8GVW_4A6wkj33yhthw&oe=62DEC24C',
+        quantity: 0,
+        cashout: 'Làng SOS',
+        price: '20.000đ',
+        total: '40.000đ',
+    },
+    {
+        name: 'Mặt nạ nghệ 123',
+        image: 'https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/295082524_2690799244386588_2912082838044312187_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=2K-jpp-1S9sAX-mte4L&_nc_ht=scontent.fhan14-1.fna&oh=00_AT-bHMdlW8GzLH9tbB0ixG4PIr2N8GVW_4A6wkj33yhthw&oe=62DEC24C',
+        quantity: 0,
+        cashout: 'Làng SOS',
+        price: '20.000đ',
+        total: '40.000đ',
+    },
+]
+
+export const dataCanceledOrder = [
+    {
+        id: '#015',
+        name: 'Loan Hoàng',
+        status: EStatusOrder.canceled,
+        total: '20.000 đ',
+        cashout: 'Làng SOS',
+        date: '03.07.2022',
+    },
+    {
+        id: '#015',
+        name: 'Loan Hoàng',
+        status: EStatusOrder.canceled,
+        total: '20.000 đ',
+        cashout: 'Làng SOS',
+        date: '03.07.2022',
+    },
+    {
+        id: '#015',
+        name: 'Loan Hoàng',
+        status: EStatusOrder.outStock,
+        total: '20.000 đ',
+        cashout: 'Làng SOS',
+        date: '03.07.2022',
+    },
+    {
+        id: '#015',
+        name: 'Loan Hoàng',
+        status: EStatusOrder.delivering,
+        total: '20.000 đ',
+        cashout: 'Làng SOS',
+        date: '03.07.2022',
+    },
+]
