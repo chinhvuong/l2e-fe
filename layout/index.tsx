a2df8770bbc4cb42bf31037872d0b843b63f570e
ducluong <luongnd@tokyotechlab.com>
2022-07-27 23:26:22 +0700

    add animation layout

--- layout/index.tsx
+++ layout/index.tsx
@@ -4,20 +4,40 @@ import Header from './header'
 import Sidebar from './sidebar'
 import Breadcrumb from './breadcumb'
 const Layout = ({ children }: { children: ReactChild }) => {
+    // const [mainHeight, setMainHeight] = useState<any>()
+
+    // useEffect(() => {
+    //     const main = document.getElementById('Main')
+    //     if (main) {
+    //         setMainHeight(main.clientHeight)
+    //         console.log(
+    //             '🚀 ~ file: index.tsx ~ line 13 ~ useEffect ~ main.clientHeight',
+    //             main.clientHeight,
+    //         )
+    //     }
+    // }, [])
+
     return (
         <div>
             <Header />
-            <main className="limit">
+            <main className={`limit sm:relative`}>
                 <Breadcrumb />
                 {/* {children} */}
                 <div className="flex sm:flex-col gap-[30px]">
-                    <div className="w-2/5 sm:w-full">
+                    <div className="w-2/5 sm:w-full" id="SideBar">
                         <Sidebar />
                     </div>
-                    <div className="w-full">{children}</div>
+                    <div
+                        className="w-full app-transition -translate-x-full left-0 main-transition h-full sm:absolute top-0 px-4 bg-white"
+                        id="Main"
+                    >
+                        {children}
+                    </div>
                 </div>
             </main>
-            <Footer />
+            <div>
+                <Footer />
+            </div>
         </div>
     )
 }
