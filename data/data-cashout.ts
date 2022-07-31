c810641758f07ff58f08cb403cfce8091fb24d76
ducluong <luongnd@tokyotechlab.com>
2022-07-31 11:44:48 +0700

    add data

---
+++ data/data-cashout.ts
@@ -0,0 +1,31 @@
+export const dataCashoutDetail = [
+    {
+        id: '#015',
+        name: 'Mặt nạ nghệ từ thiên nhiên x 1',
+        date: '03.07.2022',
+        image: 'https://avatars.mds.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d-4473719-images-taas-consumers&n=27&h=480&w=480',
+        quantity: 2,
+        customer: 'Loan Hoàng ',
+        totalAmount: '2000.00 d',
+    },
+]
+export const dataCashout = [
+    {
+        cashout: 'Làng SOS',
+        image: 'https://avatars.mds.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d-4473719-images-taas-consumers&n=27&h=480&w=480',
+        lastDate: '27/7/2022',
+        value: '2.000.000 đ',
+    },
+    {
+        cashout: 'Làng SOS',
+        image: 'https://avatars.mds.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d-4473719-images-taas-consumers&n=27&h=480&w=480',
+        lastDate: '27/7/2022',
+        value: '2.000.000 đ',
+    },
+    {
+        cashout: 'Làng SOS',
+        image: 'https://avatars.mds.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d-4473719-images-taas-consumers&n=27&h=480&w=480',
+        lastDate: '27/7/2022',
+        value: '2.000.000 đ',
+    },
+]
