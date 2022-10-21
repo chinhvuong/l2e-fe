69341610a5ba2af395ad28c6d6721ae1fd8db6b6
DESKTOP-SC5P5G9\PC <huuan152@gmail.com>
2022-10-21 15:13:46 +0700

    merge master to main, fix error

--- layout/main-layout/header/connect-wallet.tsx
+++ layout/main-layout/header/connect-wallet.tsx
@@ -1,9 +1,26 @@
-import * as React from 'react'
-
+import React from 'react'
+import Link from 'next/link' // Dynamic routing
+import { eth } from 'state/eth' // Global state
+import { useState } from 'react' // State management
+import './style.scss'
 export default function ConnectWallet() {
+    // Global state
+    const { address, unlock }: { address: string | null; unlock: Function } =
+        eth.useContainer()
+    // Action menu open state
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
     return (
         <div className="bg-primary rounded-[80px] py-[12px] px-[30px] cursor-pointer">
-            Connect Wallet
+            {/* Auth + details */}
+            {/* Unlock button */}
+            <button onClick={() => unlock()}>
+                {!address
+                    ? // If not connected, render connect wallet
+                      'Connect Wallet'
+                    : // Else, render address
+                      `${address.substr(0, 6)}...
+                      ${address.slice(address.length - 4)}`}
+            </button>
         </div>
     )
 }
--- layout/main-layout/header/index.tsx
+++ layout/main-layout/header/index.tsx
@@ -7,6 +7,7 @@ import Button from '@/components/core/button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faBell, faWallet } from '@fortawesome/free-solid-svg-icons'
 import Router from 'next/router'
+import ConnectWallet from './connect-wallet'
 
 interface IHeader {
     darkTheme: boolean
@@ -57,9 +58,7 @@ const Header = (props: IHeader) => {
                     }`}
                 />
                 {!props.isLoggedIn ? (
-                    <Button className="btn-primary under_lg:hidden">
-                        Connect Wallet
-                    </Button>
+                    <ConnectWallet />
                 ) : (
                     <div className="flex items-center justify-between sm:hidden lg:w-[150px] under_lg:hidden">
                         <FontAwesomeIcon
