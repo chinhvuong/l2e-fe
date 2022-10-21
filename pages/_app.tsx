53616f22b07dae2a931cb452588967950b3d7851
viethoa5 <viethoa5555@gmail.com>
2022-10-21 12:47:08 +0700

    fix conflict

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { appWithTranslation } from 'next-i18next'
 import '@/styles/global-style.css'
 import '@/styles/base.scss'
 import StateProvider from 'state'
+import { Provider } from 'react-redux'
 import { store } from '@/store'
 import { AppProps } from 'next/app'
 import Layout from '@/layout'
@@ -20,7 +21,11 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
     // Use the layout defined at the page level, if available
     const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>)
     return (
-        <StateProvider>{getLayout(<Component {...pageProps} />)}</StateProvider>
+        <StateProvider>
+            <Provider store={store}>
+                {getLayout(<Component {...pageProps} />)}
+            </Provider>
+        </StateProvider>
     )
 }
 
