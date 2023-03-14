133fdd566223d9f60fe8ff0be8d2169262ab17c3
viethoa5 <viethoa5555@gmail.com>
2023-03-14 16:08:52 +0700

    check merge

--- layout/questions-layout/create-questions-layout/questions-detail/header/index.tsx
+++ layout/questions-layout/create-questions-layout/questions-detail/header/index.tsx
@@ -11,7 +11,7 @@ export interface IHeaderProps {}
 export default function QuestionsHeader() {
     const router = useRouter()
     const goBack = () => {
-        router.push('/instructor')
+        router.push('/instructor/courses')
     }
 
     return (
