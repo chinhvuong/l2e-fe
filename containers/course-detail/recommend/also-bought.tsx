a75eaf77378702c5b06c4b5c4aa5aa5500b6f3c5
huuan152 <Lan.Truong@caprelo.com>
2023-03-14 20:57:46 +0700

    Fix: bugs for deployment

--- containers/course-detail/recommend/also-bought.tsx
+++ containers/course-detail/recommend/also-bought.tsx
@@ -1,5 +1,4 @@
 import VerticalCourseList from '@/components/common/vertical-course-list'
-import { dataCourses_preview_list } from '@/data/course-preview'
 import { useCourseDetailContext } from '../course-detail-context'
 
 export interface IAlsoBoughtProps {}
@@ -16,7 +15,7 @@ export default function AlsoBought() {
             <div className="font-semibold text-[26px]">
                 Students also bought
             </div>
-            <VerticalCourseList data={dataCourses_preview_list} />
+            {/* <VerticalCourseList data={dataCourses_preview_list} /> */}
         </div>
     )
 }
--- containers/course-detail/recommend/same-author-courses.tsx
+++ containers/course-detail/recommend/same-author-courses.tsx
@@ -1,5 +1,4 @@
 import VerticalCourseList from '@/components/common/vertical-course-list'
-import { dataCourses_preview_list } from '@/data/course-preview'
 import * as React from 'react'
 
 export interface ISameAuthorCoursesProps {}
@@ -11,7 +10,7 @@ export default function SameAuthorCourses() {
                 More courses by{' '}
                 <span className="text-hyperlink">Dr. Angela Vu </span>
             </div>
-            <VerticalCourseList data={dataCourses_preview_list} />
+            {/* <VerticalCourseList data={dataCourses_preview_list} /> */}
         </div>
     )
 }
