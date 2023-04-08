3f8f8fc441c415eddf8e8171f556a72d705c0afc
huuan152 <Lan.Truong@caprelo.com>
2023-04-09 00:00:26 +0700

    Fix: Connect Wallet button

--- layout/main-layout/header/wallet.tsx
+++ layout/main-layout/header/wallet.tsx
@@ -1,11 +1,22 @@
-import { useAppSelector } from '@/hooks'
+import { useAppDispatch, useAppSelector } from '@/hooks'
 import { getLoginState } from '@/store/user/selectors'
 import { ConnectWalletButton } from '@/wallet/ui'
 import Account from './account'
 import './style.scss'
+import { useEffect } from 'react'
+import { ACCESS_TOKEN } from '@/constants/localStorage'
+import { updateLoginState } from '@/store/user'
 
 export default function Wallet(props: any) {
     const loginState = useAppSelector(getLoginState)
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        if (localStorage.getItem(ACCESS_TOKEN)) {
+            dispatch(updateLoginState(true))
+        }
+    }, [loginState])
 
     if (loginState) {
         return <Account darkTheme={props.darkTheme} />
